c50a0dcf7b1cace12507d257f3c28914b90d5fb7
Albert <otec-akk@mail.ru>
2018-11-15 20:00:21 +0400

    first commit

---
+++ js/main.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+	
+	// $('#toggle').click(function () {
+	// 	$('#toggle').toggleClass('on');
+	// 	if($('#toggle').hasClass('on')){
+	// 		$('#aboutCompany').stop().fadeOut(50);
+	// 	} else {
+	// 		$('#aboutCompany').fadeIn(2000);
+	// 	}
+	// })
+
+
+
+});
