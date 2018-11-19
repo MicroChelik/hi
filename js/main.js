0dcc3eaaa2f74a3dc4a922e4bf4036ab3feedd07
Albert <otec-akk@mail.ru>
2018-11-19 18:02:54 +0400

    сделаны слайдеры, внесены некоторые правки

--- js/main.js
+++ js/main.js
@@ -1,25 +1,28 @@
 $(document).ready(function() {		
 	
-   	function windowSize(){
-    	if ($(window).width() <= '960'){
-    		$('.certificatesSlider').slick({
-			    infinite: true,
-			    slidesToShow: 1,
-			    slidesToScroll: 1,
-			    prevArrow: '.prev3',
-				nextArrow: '.next3'
-			});
-    	} else {
-    		$('.advantagesSlider').slick('unslick');
-    	}
-    }
-    $(window).on('load resize',windowSize);
-
+   // 	function windowSize(){
+   //  	if ($(window).width() <= '1200'){
+   //  		$('.certificatesSlider').slick({
+			//     infinite: true,
+			//     slidesToShow: 1,
+			//     slidesToScroll: 1,
+			//     dots: true,
+			// 	dotsClass: "my-dotsDark",
+			//     prevArrow: '.prev3',
+			// 	nextArrow: '.next3'
+			// });
+   //  	} else {
+   //  		$('.advantagesSlider').slick('unslick');
+   //  	}
+   //  }
+   //  $(window).on('load resize',windowSize);
 
   	$('.headSlider').slick({
 		infinite: true,
 		slidesToShow: 1,
 		slidesToScroll: 1,
+		dots: true,
+		dotsClass: "my-dots",
 		prevArrow: '.prev1',
 		nextArrow: '.next1'
 	});
@@ -27,9 +30,21 @@ $(document).ready(function() {
 		infinite: true,
 		slidesToShow: 3,
 		slidesToScroll: 1,
+		dots: true,
+		dotsClass: "my-dotsDark",
 		prevArrow: '.prev2',
 		nextArrow: '.next2',
 		responsive: [
+			{
+				breakpoint: 1200,
+				settings: {
+					infinite: true,
+					slidesToShow: 2,
+					slidesToScroll: 1,
+					prevArrow: '.prev2',
+					nextArrow: '.next2'
+				}
+			},
 			{
 				breakpoint: 960,
 				settings: {
@@ -42,10 +57,35 @@ $(document).ready(function() {
 			}
 		]
 	});
+	$('.certificatesSlider').slick({
+		infinite: false,
+		slidesToShow: 5,
+		slidesToScroll: 1,
+		dots: false,
+		arrows: false,
+		responsive: [
+			{
+				breakpoint: 6000,
+				settings: "unslick"
+			},
+			{
+				breakpoint: 960,
+				settings: {
+					infinite: true,
+					slidesToShow: 1,
+					slidesToScroll: 1,
+					dots: true,
+					dotsClass: "my-dotsDark",
+				}
+			}
+		]
+	});
 	$('.partnersSlider').slick({
 	    infinite: true,
 	    slidesToShow: 4,
 	    slidesToScroll: 1,
+	    dots: true,
+		dotsClass: "my-dotsDark",
 	    prevArrow: '.prev4',
 		nextArrow: '.next4',
 	    responsive: [
